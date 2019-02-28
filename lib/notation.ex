ef9f222e9f8b39a0d64950871e8fe649a868581d
Joseph Pack <josephapack@hotmail.co.uk>
2019-02-28 15:40:48 +0000

    Nested hash and array functionallity
    Co-authored-by: name: Ryan Wood ryan.wood01@bbc.co.uk

--- lib/notation.ex
+++ lib/notation.ex
@@ -15,6 +15,7 @@ defmodule Notation do
     case Util.typeof(data) do
      "binary"  -> data <> "S"
      "integer" -> Integer.to_string(data) <> "N"
+     "float"   -> Float.to_string(data) <> "N"
      "list"    -> sort_list(data) <> "A"
      "map"     -> sort_map(data) <> "H"
     end
@@ -22,15 +23,16 @@ defmodule Notation do
 
   defp sort_list(list) do
     list
-    |> Enum.sort()
+    |> Enum.sort(&string_compare/2)
+    |> IO.inspect()
     |> Enum.map(&notate/1)
     |> to_string
   end
   
   defp sort_map(map) do
     map
-    |> Enum.sort()
     |> tuple()
+    |> Enum.sort(&string_compare/2)
     |> Enum.map(&notate/1)
     |> to_string
   end
@@ -39,6 +41,35 @@ defmodule Notation do
     tuple = []
     for {k, v} <- map do
       tuple ++ [to_string(k), v]
+    end
+  end
+
+  def string_compare(nil, _) do
+    true
+  end
+
+  def string_compare(_, nil) do
+    false
+  end
+
+  def string_compare(a, b) when is_number(a) and is_bitstring(b) do
+    to_string(a) <= b
+  end
+
+  def string_compare(a, b) when is_bitstring(a) and is_number(b) do
+    a <= to_string(b)
+  end
+
+  def string_compare(a, b) do
+    compare_objects(a) <= compare_objects(b)
+  end
+
+  def compare_objects(obj) do
+    case Util.typeof(obj) do
+      "list" -> Enum.sort(obj, &string_compare/2)
+      "map"  -> Enum.sort(tuple(obj), &string_compare/2)
+      ""
+      _      -> obj
     end
   end
 end
