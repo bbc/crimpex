e345e5e322b28e23317fe49656882a686a434f4d
Joseph Pack <josephapack@hotmail.co.uk>
2019-05-16 14:34:00 +0100

    Remove uneeded utils and turn case to guards

--- lib/notation.ex
+++ lib/notation.ex
@@ -1,25 +1,19 @@
 defmodule Notation do
-  def notate(:false) do
-    "falseB"
-  end
+  def notate(:false), do: "falseB"
 
-  def notate(:true) do
-    "trueB"
-  end
+  def notate(:true), do: "trueB"
 
-  def notate(nil) do
-    "_"
-  end
+  def notate(nil), do: "_"
 
-  def notate(data) do
-    case Util.typeof(data) do
-     "binary"  -> data <> "S"
-     "integer" -> Integer.to_string(data) <> "N"
-     "float"   -> Float.to_string(data) <> "N"
-     "list"    -> sort_list(data) <> "A"
-     "map"     -> sort_map(data) <> "H"
-    end
-  end
+  def notate(data) when is_binary(data), do: data <> "S"
+
+  def notate(data) when is_integer(data), do: Integer.to_string(data) <> "N"
+
+  def notate(data) when is_float(data), do: Float.to_string(data) <> "N"
+
+  def notate(data) when is_list(data), do: sort_list(data) <> "A"
+
+  def notate(data) when is_map(data), do: sort_map(data) <> "H"
 
   defp sort_list(list) do
     list
@@ -43,32 +37,29 @@ defmodule Notation do
     end
   end
 
-  def string_compare(nil, _) do
+  defp string_compare(nil, _) do
     true
   end
 
-  def string_compare(_, nil) do
+  defp string_compare(_, nil) do
     false
   end
 
-  def string_compare(a, b) when is_number(a) and is_bitstring(b) do
+  defp string_compare(a, b) when is_number(a) and is_bitstring(b) do
     to_string(a) <= b
   end
 
-  def string_compare(a, b) when is_bitstring(a) and is_number(b) do
+  defp string_compare(a, b) when is_bitstring(a) and is_number(b) do
     a <= to_string(b)
   end
 
-  def string_compare(a, b) do
+  defp string_compare(a, b) do
     compare_objects(a) <= compare_objects(b)
   end
 
-  def compare_objects(obj) do
-    case Util.typeof(obj) do
-      "list" -> Enum.sort(obj, &string_compare/2)
-      "map"  -> Enum.sort(tuple(obj), &string_compare/2)
-      ""
-      _      -> obj
-    end
-  end
+  defp compare_objects(obj) when is_list(obj), do: Enum.sort(obj, &string_compare/2)
+
+  defp compare_objects(obj) when is_map(obj), do: Enum.sort(tuple(obj), &string_compare/2)
+
+  defp compare_objects(obj) when not is_list(obj) or is_map(obj), do: obj
 end
