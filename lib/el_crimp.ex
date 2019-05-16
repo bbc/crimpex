JoeARO/El-Crimp

Add signature functionality and link to Notation module

lib/el_crimp.ex
@@ -1,0 +1,18 @@
+defmodule ElCrimp do
+  @moduledoc """
+  Documentation for ElCrimp.
+  """
+
+  @doc """
+  """
+  def signature(data) do
+    data
+    |> Notation.notate()
+    |> md5()
+    |> Base.encode16(case: :lower)
+  end
+
+  defp md5(data) do
+    :crypto.hash(:md5, data)
+  end
+end

test/el_crimp_test.exs
@@ -1,0 +1,17 @@
+defmodule ElCrimpTest do
+  use ExUnit.Case
+
+  describe "signature" do
+    test "when given a nil value it returns the correct signature" do
+      assert ElCrimp.signature(nil) == "b14a7b8059d9c055954c92674ce60032"
+    end
+
+    test "when given a single string it returns the correct signature" do
+      assert ElCrimp.signature("abc") == "c4449120506d97975c67be69719a78e2"
+    end
+
+    test "when given a single integer it returns the correct signature" do
+      assert ElCrimp.signature(1) == "594170053719896a11eb08ee513813d5"
+    end
+  end
+end

bbc/crimpex

lib/el_crimp.ex
@@ -1,0 +1,18 @@
+defmodule Crimpex do
+  @moduledoc """
+  Documentation for Crimpex.
+  """
+
+  @doc """
+  """
+  def signature(data) do
+    data
+    |> Notation.notate()
+    |> md5()
+    |> Base.encode16(case: :lower)
+  end
+
+  defp md5(data) do
+    :crypto.hash(:md5, data)
+  end
+end

test/el_crimp_test.exs
@@ -1,0 +1,17 @@
+defmodule CrimpexTest do
+  use ExUnit.Case
+
+  describe "signature" do
+    test "when given a nil value it returns the correct signature" do
+      assert Crimpex.signature(nil) == "b14a7b8059d9c055954c92674ce60032"
+    end
+
+    test "when given a single string it returns the correct signature" do
+      assert Crimpex.signature("abc") == "c4449120506d97975c67be69719a78e2"
+    end
+
+    test "when given a single integer it returns the correct signature" do
+      assert Crimpex.signature(1) == "594170053719896a11eb08ee513813d5"
+    end
+  end
+end